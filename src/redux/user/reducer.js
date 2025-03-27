56f23cbfc8124c50453e19e4748b8bc1e787b17a
joaopedrogueds <joaopedroguedestorres@gmail.com>
2025-03-27 16:11:55 -0300

    criei o reducer

--- src/redux/user/reducer.js
+++ src/redux/user/reducer.js
@@ -1,6 +1,16 @@
-const userReducer = ()=> {
+const initialState = {
+    currentUser: null,
+};
 
-    
+
+const userReducer = (state = initialState, action)=> {
+    if(action.type ==='user/login'){
+        return {
+            ...state,
+            currentUser: 10,
+        }
+    }
+    return state;
 };
 
 
