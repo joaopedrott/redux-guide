104d547aa360d390c089d10f8ad8a7b26e1e4c3a
joaopedrogueds <joaopedroguedestorres@gmail.com>
2025-04-04 12:56:22 -0300

    anotacao

--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -17,3 +17,7 @@ const Products = () => {
 };
 
 export default Products;
+
+//Este componente pega os produtos que estao no arquivo data/products.js e renderiza eles na tela.
+//Este componente é o primeiro componente a pegar os produtos e compartilha com outro componente, neste caso o ProductItem.
+//Dentro do ProductItem, é renderizado o nome do produto, o preço e a imagem. E feito o dispatch do produto para o reducer assim que o botao eh apertado, mandando os dados do produto como action para o reducer.
