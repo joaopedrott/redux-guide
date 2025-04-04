813742f28ecf6dd992ccd0ef2cad5fb5a8e7155b
joaopedrogueds <joaopedroguedestorres@gmail.com>
2025-04-04 11:41:56 -0300

    adicionei a logica da quantidade de produtos iguais no reducer da pasta cart

--- src/redux/cart/reducer.js
+++ src/redux/cart/reducer.js
@@ -8,10 +8,18 @@ const initialState = {
 const cartReducer = (state= initialState, action)=> {
     switch(action.type) {
         case CartActionnTypes.ADD_PRODUCT:
-            return {
-                ...state,
-                products: [...state.products, action.payload],
+            //verificar se o produto já existe no carrinho
+            const productIsAlreadyInCart = state.products.some(product => product.id === action.payload.id)
+            
+            //se ele existir, aumentar a quantidade em 1
+            if(productIsAlreadyInCart) {
+                return{...state, products: state.products.map(product => product.id === action.payload.id ? {...product, quantity: product.quantity + 1} : product),
+
+                };
             }
+
+            //se ele não existir, adicionar o produto no carrinho
+            return{...state, products: [...state.products, {...action.payload, quantity: 1}]};
         default:
         return state;
     }
