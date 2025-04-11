78a63b8e5742759ccdf79d01d2c91faaa78ff474
joaopedrogueds <joaopedroguedestorres@gmail.com>
2025-04-11 11:56:20 -0300

    Update index.jsx

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 // Components
 import Cart from "../cart/index";
