3f8ad23d339e9a7017f38db3febd25b26c173ac4
joaopedrogueds <joaopedroguedestorres@gmail.com>
2025-04-14 10:34:46 -0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
-
+//teste
 const store = createStore(rootReducer, applyMiddleware(logger));
 
 export default store;
